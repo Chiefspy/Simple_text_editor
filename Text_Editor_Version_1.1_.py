f9a670b00acee3978d39bae87a2c6427ef2524ed
Chiefspy <165175642+Chiefspy@users.noreply.github.com>
2024-07-06 10:59:21 -0400

    Add files via upload

--- Text_Editor_Version_1.1_.py
+++ Text_Editor_Version_1.1_.py
@@ -3,13 +3,13 @@ from tkinter import filedialog
 from tkinter import messagebox
 
 
-def copy():
+def copy(text):
     copied_text = text.get(1.0, END)
 
     return copied_text
 
 
-def delete():
+def delete(text):
     if messagebox.askyesno(title="askyesno",
                            message="Are you sure you want to delete all text inside the text area? All unsaved progress will be lost"):
         text.delete(1.0, END)
@@ -17,11 +17,11 @@ def delete():
         return
 
 
-def paste(copied_text):
+def paste(copied_text, text):
     text.insert(INSERT, copied_text)
 
 
-def saveFile(types):
+def saveFile(types, text):
     file = filedialog.asksaveasfile(title="ask save as file",
                                     initialdir=r"\home",
                                     filetypes=types)
@@ -31,7 +31,7 @@ def saveFile(types):
     file.close()
 
 
-def openFile(types):
+def openFile(types, text):
     try:
         filepath = str(filedialog.askopenfilename(title="ask open file name",
                                                   initialdir=r"/home",
@@ -42,19 +42,19 @@ def openFile(types):
         messagebox.showerror(title="show error", message=f"{e}")
 
 
-def closeFile():
+def closeFile(window):
     if messagebox.askyesno(title="ask yes no",
                            message="Are you sure you want to exit? All unsaved progress will be lost."):
         window.destroy()
     else:
         return
 
+def main():
+    window = Tk()
 
-window = Tk()
+    window.title("Simple Text Editor")
 
-window.title("Simple Text Editor")
-
-filetypes = [("All files", "*.*"),
+    filetypes = [("All files", "*.*"),
              ("Python file", "*.py"),
              ("Text file", "*.txt"),
              ("HTML file", "*.html"),
@@ -62,40 +62,43 @@ filetypes = [("All files", "*.*"),
              ("C file", "*.c")]
 
 
-saveImage = PhotoImage(file="FlopyDisk.png")
-openImage = PhotoImage(file="Folder.png")
-exitImage = PhotoImage(file="Stop.png")
+    saveImage = PhotoImage(file="FlopyDisk.png")
+    openImage = PhotoImage(file="Folder.png")
+    exitImage = PhotoImage(file="Stop.png")
 
 
-deleteImage = PhotoImage(file="Delete.png")
-copyImage = PhotoImage(file="Copy.png")
-pasteImage = PhotoImage(file="paste.png")
+    deleteImage = PhotoImage(file="Delete.png")
+    copyImage = PhotoImage(file="Copy.png")
+    pasteImage = PhotoImage(file="paste.png")
 
-menuBar = Menu(window, )
-window.config(menu=menuBar)
+    menuBar = Menu(window, )
+    window.config(menu=menuBar)
 
-fileMenu = Menu(menuBar, tearoff=0)
-menuBar.add_cascade(menu=fileMenu, label="file")
+    fileMenu = Menu(menuBar, tearoff=0)
+    menuBar.add_cascade(menu=fileMenu, label="file")
 
-fileMenu.add_command(label="save", command=lambda: saveFile(filetypes), image=saveImage, compound=LEFT)
-fileMenu.add_command(label="Open", command=lambda: openFile(filetypes), image=openImage, compound=LEFT)
+    fileMenu.add_command(label="save", command=lambda: saveFile(filetypes, text), image=saveImage, compound=LEFT)
+    fileMenu.add_command(label="Open", command=lambda: openFile(filetypes, text), image=openImage, compound=LEFT)
 
-fileMenu.add_separator()
-fileMenu.add_command(label="Exit", command=closeFile, image=exitImage, compound=LEFT)
+    fileMenu.add_separator()
+    fileMenu.add_command(label="Exit", command=lambda: closeFile(window), image=exitImage, compound=LEFT)
 
-editMenu = Menu(menuBar, tearoff=0)
-menuBar.add_cascade(menu=editMenu, label="edit")
+    editMenu = Menu(menuBar, tearoff=0)
+    menuBar.add_cascade(menu=editMenu, label="edit")
 
-editMenu.add_command(label="Copy", command=copy, compound=LEFT, image=copyImage)
-editMenu.add_command(label="Paste", command=lambda: paste(copy()), compound=LEFT, image=pasteImage)
+    editMenu.add_command(label="Copy", command=lambda: copy(text), compound=LEFT, image=copyImage)
+    editMenu.add_command(label="Paste", command=lambda: paste(copy(), text), compound=LEFT, image=pasteImage)
 
-editMenu.add_separator()
+    editMenu.add_separator()
 
-editMenu.add_command(label="delete", command=delete, compound=LEFT, image=deleteImage)
-
-
-text = Text(window, padx=20, pady=20, font=("Ink free", 20), width=40, height=20)
-text.pack()
+    editMenu.add_command(label="delete", command=lambda: delete(text), compound=LEFT, image=deleteImage)
 
 
-window.mainloop()
+    text = Text(window, padx=20, pady=20, font=("Ink free", 20), width=40, height=20)
+    text.pack()
+
+
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
