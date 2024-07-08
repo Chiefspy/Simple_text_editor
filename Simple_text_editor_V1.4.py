718bb5dc9a83cfbdadda3364d62873d91a9cd12e
Chiefspy <165175642+Chiefspy@users.noreply.github.com>
2024-07-08 16:11:00 -0400

    Add files via upload

---
+++ Simple_text_editor_V1.4.py
@@ -0,0 +1,133 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import ttk
+import os
+
+
+def copy(text):
+    copied_text = text.get(1.0, END)
+
+    return copied_text
+
+
+def delete(text):
+    if messagebox.askyesno(title="askyesno",
+                           message="Are you sure you want to delete all text inside the text area? All unsaved progress will be lost"):
+        text.delete(1.0, END)
+    else:
+        return
+
+
+def paste(copied_text, text):
+    text.insert(INSERT, copied_text)
+
+
+def saveFile(types, text):
+    file = filedialog.asksaveasfile(title="ask save as file",
+                                    initialdir=r"\home",
+                                    filetypes=types)
+    if file is None:
+        return
+    file.write(text.get(1.0, END))
+    file.close()
+
+
+def openFile(types, text):
+    try:
+        filepath = str(filedialog.askopenfilename(title="ask open file name",
+                                                  initialdir=r"/home",
+                                                  filetypes=types))
+        with open(filepath, 'r') as f:
+            text.insert(INSERT, f.read())
+    except FileNotFoundError as e:
+        messagebox.showerror(title="show error", message=f"{e}")
+
+
+def closeFile(window):
+    if messagebox.askyesno(title="ask yes no",
+                           message="Are you sure you want to exit? All unsaved progress will be lost."):
+        window.destroy()
+    else:
+        return
+
+def create_window(window):
+    new_window = Toplevel(window)
+    notebook = ttk.Notebook(new_window,)
+    tab1 = Frame(notebook,)
+    tab2 = Frame(notebook,)
+    notebook.add(tab1, text="tab1")
+    notebook.add(tab2, text="tab2")
+    notebook.pack()
+    Label(tab1, text="Hello", width=50, height=25).pack()
+    Label(tab2, text="Goodbye", width=50, height=25).pack()
+
+    #Feature not complete 
+
+def main():
+    window = Tk()
+
+    window.title("Simple Text Editor")
+
+    home_dir = os.path.expanduser('~')
+
+
+    filetypes = [("All files", "*.*"),
+             ("Python file", "*.py"),
+             ("Text file", "*.txt"),
+             ("HTML file", "*.html"),
+             ("Javascript file", "*.js"),
+             ("C file", "*.c")]
+
+
+
+    saveImage = PhotoImage(file=rf"{home_dir}/Simple_text_editor/FlopyDisk.png")
+    openImage = PhotoImage(file=rf"{home_dir}/Simple_text_editor/Folder.png")
+    exitImage = PhotoImage(file=rf"{home_dir}/Simple_text_editor/Stop.png")
+
+    deleteImage = PhotoImage(file=rf"{home_dir}/Simple_text_editor/Delete.png")
+    copyImage = PhotoImage(file=rf"{home_dir}/Simple_text_editor/Copy.png")
+    pasteImage = PhotoImage(file=rf"{home_dir}/Simple_text_editor/paste.png")
+
+    create_WindowImage = PhotoImage(file=rf"{home_dir}/Simple_text_editor/new_tab_icon.png")
+
+
+    menuBar = Menu(window, )
+    window.config(menu=menuBar)
+
+    fileMenu = Menu(menuBar, tearoff=0)
+    menuBar.add_cascade(menu=fileMenu, label="file")
+
+    fileMenu.add_command(label="save", command=lambda: saveFile(filetypes, text), image=saveImage, compound=LEFT)
+    fileMenu.add_command(label="Open", command=lambda: openFile(filetypes, text), image=openImage, compound=LEFT)
+
+    fileMenu.add_separator()
+    fileMenu.add_command(label="Exit", command=lambda: closeFile(window), image=exitImage, compound=LEFT)
+
+    editMenu = Menu(menuBar, tearoff=0)
+    menuBar.add_cascade(menu=editMenu, label="edit")
+
+    editMenu.add_command(label="Copy", command=lambda: copy(text), compound=LEFT, image=copyImage)
+    editMenu.add_command(label="Paste", command=lambda: paste(copy(text), text), compound=LEFT, image=pasteImage)
+
+    editMenu.add_separator()
+
+    editMenu.add_command(label="delete", command=lambda: delete(text), compound=LEFT, image=deleteImage)
+
+    optionsMenu = Menu(menuBar, tearoff=0)
+    menuBar.add_cascade(menu=optionsMenu, label="options")
+
+    optionsMenu.add_command(label="Create new window", command=lambda: create_window(window), compound=LEFT, image=create_WindowImage)
+
+    text = Text(window, padx=20, pady=20, font=("Ink free", 20), width=40, height=20)
+    text.pack(expand=True, fill="both")
+
+
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
