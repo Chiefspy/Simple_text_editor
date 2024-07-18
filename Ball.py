2fbb6b7f9ff256628a41d032938c46fd06a4e638
Chiefspy <165175642+Chiefspy@users.noreply.github.com>
2024-07-18 11:23:12 -0400

    Update Ball.py

--- Ball.py
+++ Ball.py
@@ -1,6 +1,9 @@
 class Ball:
-  def __init__(self):
-    pass
+  def __init__(self, canvas, x, y, diameter, xVelocity, yVelocity, color):
+    self.canvas = canvas
+    self.image = canvas.create_oval(x, y, diameter, diameter, fill=color)
+    self.xVelocity = xVelocity
+    self.yVelocity = yVelocity
   def move(self):
     pass
     
