676a91582fddd0efd698422cfbb19f3661827569
Chiefspy <165175642+Chiefspy@users.noreply.github.com>
2024-07-20 12:21:05 -0400

    Update Simple_text_editor_V1.45.py

--- Simple_text_editor_V1.45.py
+++ Simple_text_editor_V1.45.py
@@ -68,13 +68,13 @@ def update(window, time_label):
 def create_time_window(window):
     time_window = Toplevel(window)
 
-    time_label = Label(window, font("Arial", 50), fg="#00FF00", bg="black")
+    time_label = Label(window, font=("Arial", 50), fg="#00FF00", bg="black")
     time_label.pack()
 
-    day_label = Label(window, font("Ink Free", 25))
+    day_label = Label(window, font=("Ink Free", 25))
     day_label.pack()
 
-    date_label = Label(window, font("Ink Free", 30))
+    date_label = Label(window, font=("Ink Free", 30))
     date_label.pack()
 
 
