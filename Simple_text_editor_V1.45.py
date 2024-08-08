25fbaf9776e1b5ee35596784df9020f7e74b38b2
Chiefspy <165175642+Chiefspy@users.noreply.github.com>
2024-08-08 15:36:32 -0400

    Update Simple_text_editor_V1.45.py

--- Simple_text_editor_V1.45.py
+++ Simple_text_editor_V1.45.py
@@ -446,7 +446,7 @@ def main():
     color_button = Button(optionsFrame, text="color", command=lambda: change_color(text))
     color_button.pack(side=LEFT)
 
-    font_box = OptionMenu(optionsFrame, font_name, *font.families(), command=lambda : change_font(text, font_name))
+    font_box = OptionMenu(optionsFrame, font_name, *font.families(), command=lambda font_name: change_font(text, font_name))
     font_box.pack(side=LEFT)
 
     size_box = Spinbox(optionsFrame, from_=1, to=100, textvariable=font_size, command=lambda: change_font(text, font_size))
